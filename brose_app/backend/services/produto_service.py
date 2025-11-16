1fd82761f5ed70ee282a1f8bc424801e652190e4
ArthDroid1 <barbosarthur7@gmail.com>
2025-11-16 17:51:16 -0300

    Cria método excluir produto

--- brose_app/backend/services/produto_service.py
+++ brose_app/backend/services/produto_service.py
@@ -1,12 +1,12 @@
 from repositories.produto_repository import ProdutoRepository
+from ..models.database import db
 from ..models.Produto_model import Produto
 
 class ProdutoService:
-
     def __init__(self):
         self.repository = ProdutoRepository()
-
-    def cadastrar_produto(self, dados: dict):
+        
+    def cadastrar_produto(dados: dict):
         """Função responsável por cadastrar um novo produto no banco."""
         nome = dados.get('nome')
         quantidade = dados.get('quantidade', 0)
@@ -17,10 +17,19 @@ class ProdutoService:
             quantidade=quantidade,
             descricao=descricao
         )
+        # Chama o repositório para salvar no banco
+        return self.repository.add(produto=novo_produto)
 
-        # usa o repositório para salvar
-        return self.repository.add(novo_produto)
+    def deletar_produto(self, id_produto: int) -> bool:
+        """Remove um produto do banco de dados."""
+        # Verifica antes se existe
+        produto = self.repository.get_by_id(id_produto)
 
-    def editar_quantidade(self, dados: dict):
+        if not produto:
+            raise ValueError("Produto não encontrado.")
+
+        return self.repository.delete(id_produto)
+
+
+    def editar_quantidade(dados: dict):
         """Função responsável por editar quantidades no banco de dados"""
-        pass
