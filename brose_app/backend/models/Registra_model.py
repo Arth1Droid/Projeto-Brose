47635ade105ef9ab96f3e3f226deb4a55f331037
Beatriz_Cristiny <biacriativa124@gmail.com>
2025-11-30 12:28:29 -0300

    Ajuste nos horários que estavam com fusos errados

--- brose_app/backend/models/Registra_model.py
+++ brose_app/backend/models/Registra_model.py
@@ -1,17 +1,21 @@
 from brose_app.backend.models.database import db
+import datetime 
+import pytz
+
+FUSO_UTC = pytz.utc
 
 class Registra(db.Model):
     __tablename__ = 'registra'
 
     id_registro = db.Column(db.Integer, primary_key=True)
     id_produto_fk = db.Column(db.Integer, db.ForeignKey('produto.id_produto', ondelete='CASCADE'), nullable=False)
-    data_registro = db.Column(db.DateTime, default=db.func.current_timestamp())
+    data_registro = db.Column(db.DateTime, default=lambda: datetime.datetime.now(FUSO_UTC))
 
     def to_json(self):
         return {
             "id": self.id_registro,
             "produto_id": self.id_produto_fk,
-            "data_registro": self.data_registro
+            "data_registro": self.data_registro.isoformat()
         }
 
 #sem id_câmera porque é irrelevante pra função de contagem.
